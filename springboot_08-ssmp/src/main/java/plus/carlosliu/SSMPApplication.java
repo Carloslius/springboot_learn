25faa3b141d4288aab2c953a04fd3a0818b42643
“Carlosliu” <1753459461@qq.com>
2021-11-28 18:50:13 +0800

    启动boot程序时断开读取外部临时配置对应的入口

--- springboot_08-ssmp/src/main/java/plus/carlosliu/SSMPApplication.java
+++ springboot_08-ssmp/src/main/java/plus/carlosliu/SSMPApplication.java
@@ -7,7 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SSMPApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SSMPApplication.class, args);
+//        String[] arg = new String[1];
+//        arg[0] = "--server.port=8888";
+//        SpringApplication.run(SSMPApplication.class, arg);
+        // 可以在启动boot程序时断开读取外部临时配置对应的入口，也就是去掉读取外部参数的形参
+        SpringApplication.run(SSMPApplication.class);
     }
 
 }
